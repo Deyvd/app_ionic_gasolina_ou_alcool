877561e453fa885cd93acd556743b42cdfec506c
Marcos Calixto <marcos.calixto@huntercar.app>
2021-12-06 14:50:30 -0300

    aplicação finalizada

--- gasAlcoolApp/src/app/home/home.page.ts
+++ gasAlcoolApp/src/app/home/home.page.ts
@@ -9,4 +9,23 @@ export class HomePage {
 
   constructor() {}
 
+  public precoAlcool: any
+  public precoGasolina: any
+
+  public resultado: String = "Resultado"
+
+  calcular() {
+    if( this.precoAlcool && this.precoGasolina ) {
+      var pAlcool = parseFloat(this.precoAlcool)
+      var pGasolina = parseFloat(this.precoGasolina)
+      var calculo = pAlcool / pGasolina
+      if (calculo >=0.7) {
+        this.resultado = "Melhor usar Gasolina!";
+      } else {
+        this.resultado = "Melhor utilizar Álcool!"
+      } 
+    } else {
+      this.resultado = "Preencha corretamente os campos!"
+    }
+  }
 }
